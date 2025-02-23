c67f381bf8605a4f97c68b1b6a76007436fb62f6
simonacorreale <simona.correale.work@gmail.com>
2025-02-23 19:07:48 +0100

    fix website

--- script.js
+++ script.js
@@ -12,3 +12,46 @@ panelArray.forEach((element, index) => {
         element.classList.toggle("open");
     });
 });
+
+// element
+let button = document.querySelector(".primary-button");
+let item = document.querySelector(".primary-button .round");
+
+button.addEventListener("mouseenter", function (event) {
+    this.classList.add("animate"); // Cambiato da '+=' a 'add()' per evitare duplicazione
+
+    let buttonX = event.offsetX;
+    let buttonY = event.offsetY;
+
+    if (buttonY < 24) {
+        item.style.top = 0 + "px";
+    } else if (buttonY > 30) {
+        item.style.top = 48 + "px";
+    }
+
+    item.style.left = buttonX + "px";
+    item.style.width = "1px";
+    item.style.height = "1px";
+});
+
+button.addEventListener("mouseleave", function (event) {
+    this.classList.remove("animate");
+
+    let buttonX = event.offsetX;
+    let buttonY = event.offsetY;
+
+    // Reset delle proprietà per evitare che rimangano state precedenti
+    item.style.top = "";
+    item.style.left = "";
+    item.style.width = "";
+    item.style.height = "";
+
+    // Ridisegnare la posizione, ma resetta le dimensioni
+    if (buttonY < 24) {
+        item.style.top = 0 + "px";
+    } else if (buttonY > 30) {
+        item.style.top = 48 + "px";
+    }
+
+    item.style.left = buttonX + "px";
+});
